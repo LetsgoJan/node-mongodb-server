26176b0bb06cbf87a55ff4a60e0d08141321bfb7
jan van der want <jvdwant@hotmail.com>
2017-11-28 12:22:25 +0100

    update

---
+++ api/ingredient.routes.v1.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var routes = express.Router();
+var mongodb = require('../config/mongo.db');
+var User = require('../model/user.model');
+
+routes.post('/ingredient', function (res, req) {
+    res.contentType('application/json');
+    const ingredientProps = req.body;
+
+    User.create(ingredientProps)
+        .then(ingredient => res.send(ingredient));
+});
+
+module.exports = routes;
