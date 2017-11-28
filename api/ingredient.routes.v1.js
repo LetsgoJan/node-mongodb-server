ee2f0815689ed98518b7c60f12a3ff17b6a067af
jan van der want <jvdwant@hotmail.com>
2017-11-28 13:22:52 +0100

    update

--- api/ingredient.routes.v1.js
+++ api/ingredient.routes.v1.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var routes = express.Router();
 var mongodb = require('../config/mongo.db');
-var User = require('../model/user.model');
+var Ingredient = require('../model/ingredient.model');
 
 routes.post('/ingredient', function (req, res) {
     res.contentType('application/json');
     const ingredientProps = req.body;
 
-    User.create(ingredientProps)
+    Ingredient.create(ingredientProps)
         .then(ingredient => res.send(ingredient));
+});
+
+routes.get('/ingredient/:id', function (req, res) {
+    res.contentType('application/json');
+    const ingredientProps = req.body;
+    const ingredientId = req.params.id;
+
+    Ingredient.findByIdAndUpdate({_id : ingredientId}, ingredientProps)
+        .then(() => Ingredient.findbyid({_id : ingredientId}))
+        .then((ingredient) => res.send(ingredient));
 });
 
 module.exports = routes;
